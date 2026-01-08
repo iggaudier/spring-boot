d82dd86b59d47729c9ab37742954ac31e0f29912
iggauder <ggaudier.ian@gmail.com>
2026-01-08 23:39:25 +0800

    feat : added model and controller for Software Engineer

---
+++ src/main/java/com/iggaudier/springboot/SoftwareEngineerController.java
@@ -0,0 +1,28 @@
+package com.iggaudier.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers(){
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "James",
+                        "java, node, react, php"
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "Dianne",
+                        "javascript, angular, ruby"
+                )
+        );
+    }
+}
